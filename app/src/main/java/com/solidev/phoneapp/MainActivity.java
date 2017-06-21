7df8f9470c0e7e85ea3c0756833887a5a6c850a1
DESKTOP-5OD5J8O\Admin <thehai.se.uit@gmail.com>
2017-06-21 19:54:16 +0700

    PhoneAppDemo Upload android code

---
+++ app/src/main/java/com/solidev/phoneapp/MainActivity.java
@@ -0,0 +1,170 @@
+package com.solidev.phoneapp;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView btnSpeak, icMenu;
+    private EditText inputet;
+    private DrawerLayout drawerLayout;
+    private WebView wv1;
+    private TextView hidenView;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSpeak = (ImageView) findViewById(R.id.btnSpeak);
+        inputet = (EditText) findViewById(R.id.inputet);
+        icMenu = (ImageView) findViewById(R.id.ic_menu);
+        drawerLayout = (DrawerLayout) findViewById(R.id.activity_home_drawer_layout);
+        hidenView = (TextView) findViewById(R.id.hidenView);
+
+       hidenView.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               hideKeyboardFrom();
+           }
+       });
+
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                promptSpeechInput();
+            }
+        });
+        icMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(Gravity.RIGHT);
+            }
+        });
+
+        wv1=(WebView)findViewById(R.id.webview);
+        wv1.setWebViewClient(new MyBrowser());
+
+        inputet.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    wv1.getSettings().setLoadsImagesAutomatically(true);
+                    wv1.getSettings().setJavaScriptEnabled(true);
+                    wv1.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+
+                    String url = inputet.getText().toString();
+                    url = (url.contains("http://www."))? url : "http://www." + url;
+
+                    wv1.loadUrl(url);
+                    ChangeView();
+                    hideKeyboardFrom();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                    String url = result.get(0);
+                    url = url.replaceAll("\\s","");
+                    url = (url.contains(".")) ? "http://www." + url :"http://www." + url + ".com";
+
+                    wv1.getSettings().setLoadsImagesAutomatically(true);
+                    wv1.getSettings().setJavaScriptEnabled(true);
+                    wv1.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+                    wv1.loadUrl(url);
+                    ChangeView();
+                    inputet.setText(url);
+                }
+                break;
+            }
+
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void hideKeyboardFrom() {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    private void ChangeView(){
+        hidenView.setVisibility(View.GONE);
+        wv1.setVisibility(View.VISIBLE);
+    }
+
+}
